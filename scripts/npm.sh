d7ad078a812e720de318ff1dc9668ab7a72e1923
Esin Saribudak <esin.saribudak@generalassemb.ly>
2021-01-26 14:05:35 -0600

    Update npm.sh

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+
 # install these npm packages globally
-npm install --global npm jsonlint eslint grunt-cli remark-lint phantomjs-prebuilt nodemon node-sass
-# Don't allow devs to "fix" npm vulnerabilities for now
-npm set audit false
+if which node > /dev/null
+  # if node is installed install npm globally  
+  then
+    npm install --global npm jsonlint eslint remark-lint nodemon node-sass
+    # Don't allow devs to "fix" npm vulnerabilities for now
+    npm set audit false
+  else 
+    echo "Node is not installed, visit: https://nodejs.org and install the LTS for your system"
+    echo "After installation, rerun this script"
+fi
