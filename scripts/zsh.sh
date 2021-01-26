558d287a01a85ac1d26cae796229e33dae05f47b
Esin Saribudak <esin.saribudak@generalassemb.ly>
2021-01-26 13:14:56 -0600

    Add prompt configuration to zshrc set up

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -21,6 +21,32 @@ if [[ $(uname -s) = 'Darwin' ]]; then
   # create .zshenc file in home directory
   touch ~/.zshenv
 
+  # look for the following string in .zshrc
+  if grep -Fq "autoload -Uz vcs_info" ~/.zshrc
+  then
+  # if function is in .zshrc, nothing needs to be done
+  echo 'Git preferences for zsh already added to ~/.zshrc'
+  else
+  # otherwise, add the following to .zshrc to display git branch information
+  # vs code set as default editor
+  cat <<'EOF' >> ~/.zshrc
+  # Git function: https://stackoverflow.com/questions/59009508/how-to-only-show-current-folder-and-git-branch-and-for-home-in-zsh
+  # load version control information
+  autoload -Uz vcs_info
+  precmd() { vcs_info }
+  # format vcs_info variable
+  zstyle ':vcs_info:git:*' formats '- %F{green}%b%f'
+  # set up the prompt to include git branch and complete working directory
+  # zsh documentation: http://zsh.sourceforge.net/Doc/Release/Prompt-Expansion.html
+  setopt PROMPT_SUBST
+  NEWLINE=$'\n'
+  PROMPT='%B%F{blue}%~ ${vcs_info_msg_0_}%f%b${NEWLINE}$ '
+  # set VS code as default editor
+  export EDITOR='code --wait'
+  export VISUAL='code --wait'
+  EOF
+  fi
+
   if [[ -f ~/.zshenv && -f ~/.zshrc ]]; then
     # output success message to terminal
     echo $'\nLooks good!'
@@ -37,3 +63,5 @@ else
 
   echo 'All set. Move on to the next section.'
 fi
+
+
